e25aaf37fa2f054e8bb84f158181675a416a78d1
stijns96 <47820758+stijns96@users.noreply.github.com>
2020-12-04 15:25:42 +0100

    Homepage V2

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// document.addEventListener('DOMContentLoaded', (event) => {
+// 	getHeaderHeight = () => {
+// 		let headerHeight = document.querySelector('header').offsetHeight,
+// 			mainMargin = (document.querySelector(
+// 				'main'
+// 			).style.marginTop = `${headerHeight}px`);
+// 	};
+
+// 	getHeaderHeight();
+// });
