f83087ef6dbb4c288f2ed0498d631d6a2df1a9f3
stijns96 <47820758+stijns96@users.noreply.github.com>
2020-12-08 14:53:13 +0100

    Updated cards

--- components/Services/Services.vue
+++ components/Services/Services.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="services text-center lg:flex lg:space-x-2">
     <Service
-      class="flex-1"
+      class="flex-1 mb-2 lg:mb-0"
       icon="search"
       heading="Research"
       text="Our members are UX researchers by heart. They dive into design questions and place themselves in your users shoes. We promise that as a client you'll get a fresh angle."
@@ -10,14 +10,14 @@
     </Service>
 
     <Service
-      class="flex-1"
+      class="flex-1 mb-2 lg:mb-0"
       icon="paint-brush"
       heading="UX/UI Design"
       text="From wireframes to high-fidelity prototypes, we design digital interfaces for everyone."
     />
 
     <Service
-      class="flex-1"
+      class="flex-1 mb-2 lg:mb-0"
       icon="code"
       heading="Web Development"
       text="We specialize in translating designs pixel-perfectly into modern JamStack websites."
