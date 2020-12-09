a4bf62464682c5515e0b3aaaca2c867705319d4e
stijns96 <47820758+stijns96@users.noreply.github.com>
2020-12-09 12:43:06 +0100

    Added site header v1

--- components/Hamburger/Hamburger.vue
+++ components/Hamburger/Hamburger.vue
@@ -1,10 +1,32 @@
-<template><tasty-burger-button :type="type"/></template>
+<template>
+  <div
+    id="burger-wrapper"
+    class="w-3.5 h-3.5 flex justify-center items-center bg-primary rounded-full"
+  >
+    <tasty-burger-button
+      :type="type"
+      :size="size"
+      :color="color"
+      :active-color="activeColor"
+      @click="modalToggle"
+    />
+  </div>
+</template>
+
 <script>
 export default {
   data() {
     return {
-      type: "3dx"
+      type: "3dx",
+      size: "s",
+      color: "#f2f2f2",
+      activeColor: "#f2f2f2"
     };
+  },
+  methods: {
+    modalToggle() {
+      this.$emit("toggle");
+    }
   }
 };
 </script>
