6238283c748c2feff4f342239de6925721679320
Tim van den Bosch <mondayrunner@users.noreply.github.com>
2022-09-06 16:18:44 +0200

    Added case tiles

--- pages/work.vue
+++ pages/work.vue
@@ -5,7 +5,7 @@
     <section id="hero">
       <div class="container">
         <h1 class="mb-2">Our work</h1>
-        <p class="md:text-2xl mx-auto">A quick selection of projects</p>
+        <p class="md:text-2xl mx-auto">A quick overview of projects</p>
       </div>
     </section>
 
@@ -14,14 +14,13 @@
       <div class="container">
 
         <div class="flex">
-
           <div class="p-1">
             <img src="~/assets/images/work/joint-cyber-range.jpg"  class="block pt-2 pb-2">
-            <h3>JointCyberRange - <a href="https://jointcyberrange.nl">Visit website</a></h3>
+            <h3>JointCyberRange - <a href="https://jointcyberrange.nl">Website</a></h3>
             <p>Giving Cyber Security education a fresh look</p>
             <p class="pt-1">
               <strong class="pr-2">Research</strong>
-              <strong class="pr-2">UX/UI Design</strong>
+              <strong class="pr-2">Web Design</strong>
               <strong>Development</strong>
             </p>
           </div>
@@ -38,9 +37,133 @@
           </div>
         </div>
 
+        <div class="flex justify-center p-1">
+
+          <div class="w-1/3 flex justify-center flex-col">
+            <h2 class="mb-1 text-left text-secondary">MKB Digitaal  - <a href="https://www.mkbdigitaal.com/nieuwe-aanvraag" class="text-secondary">Website</a></h2>
+              <p>UX redesign and explanation animation</p>
+              <p class="pt-1">
+                <strong class="pr-2">Web Design</strong>
+                <strong class="pr-2">Animation</strong>
+              </p>
+          </div>
+
+          <div class="w-2/3" style="padding:56.25% 0 0 0;position:relative;">
+            <iframe src="https://player.vimeo.com/video/656621470?h=e5c38a7116&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="MKB overview4 - without ministrymp4"></iframe>
+          </div>
+        </div>
+
+        <div class="flex">
+          <div class="p-1">
+            <img src="~/assets/images/work/gemeentearchief.jpg"  class="block pt-2 pb-2">
+            <h3>Haags Gemeentearchief - <a href="https://haagsgemeentearchief.nl/">Website</a></h3>
+            <p>UX research and testing to improve their website</p>
+            <p class="pt-1">
+              <strong class="pr-2">Research</strong>
+              <strong class="pr-2">UX/UI Design</strong>
+              <strong>Test report</strong>
+            </p>
+          </div>
+
+          <div class="p-1">
+            <img src="~/assets/images/work/germandesk.jpg"  class="block pt-2 pb-2">
+            <h3>German Desk - COMING SOON</a></h3>
+            <p>Website design and development</p>
+            <p class="pt-1">
+              <strong class="pr-2">Web Design</strong>
+              <strong class="pr-2">Development</strong>
+              <strong>CMS</strong>
+            </p>
+          </div>
+        </div>
+
+        <div class="flex">
+          <div class="p-1">
+            <img src="~/assets/images/work/eu-impacthub.jpg"  class="block pt-2 pb-2">
+            <h3>European impact HUB - <a href="https://europeanimpacthub.nl/">Website</a></h3>
+            <p>Website design and development</p>
+            <p class="pt-1">
+              <strong class="pr-2">Web Design</strong>
+              <strong class="pr-2">Development</strong>
+              <strong>CMS</strong>
+            </p>
+          </div>
+
+          <div class="p-1">
+            <img src="~/assets/images/work/colosseus.jpg"  class="block pt-2 pb-2">
+            <h3>Colosseus - <a href="https://colosseus.nl/">Website</a></h3>
+            <p>Website redesign and development</p>
+            <p class="pt-1">
+              <strong class="pr-2">Web Design</strong>
+              <strong class="pr-2">Development</strong>
+              <strong>CMS</strong>
+            </p>
+          </div>
+        </div>
+
+        <div class="flex justify-center p-1">
+
+          <div class="w-2/3" style="padding:56.25% 0 0 0;position:relative;">
+            <iframe src="https://player.vimeo.com/video/668163570?h=46f27d4b22&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="MKB overview4 - without ministrymp4"></iframe>
+          </div>
+
+          <div class="w-1/3 ml-2 flex justify-center flex-col">
+            <h2 class="mb-1 text-left text-secondary">NLinBusiness</h2>
+              <p>Presenting Dutch innovative solutions on international platform and expos</p>
+              <p class="pt-1">
+                <strong class="pr-2">Video</strong>
+              </p>
+          </div>
+
+
+        </div>
+
+        <div class="flex">
+          <div class="p-1">
+            <img src="~/assets/images/work/applied-research-project.jpg"  class="block pt-2 pb-2">
+            <h3>Applied Research Project - <a href="https://appliedresearchlaw.com/">Website</a></h3>
+            <p>Website design and development</p>
+            <p class="pt-1">
+              <strong class="pr-2">Web Design</strong>
+              <strong class="pr-2">Development</strong>
+              <strong class="pr-2">CMS</strong>
+
+            </p>
+          </div>
+
+          <div class="p-1">
+            <img src="~/assets/images/work/ads-ai.jpg"  class="block pt-2 pb-2">
+            <h3>Applied Data Science & Artificial Intelligence</h3>
+            <p>Branding material</p>
+            <p class="pt-1">
+              <strong class="pr-2">Branding</strong>
+              <strong class="pr-2">Graphic Design</strong>
+            </p>
+          </div>
+        </div>
+
+<!-- Next Fashion Retail
+Inspriratiepad
+
+MyTimetable. TermTime Tutorials
+
+WE-IT
+TNO Wegwijzer Circulaire Maakindustrie
+Instellingsplan
+Applied Research Projects
+CareerCrafting
+THUAS ADS&AI
+Applied Research Project
+MKB Digiwerkplaats
+National CMD Day
+ -->
       </div>
+
     </section>
+    <script src="https://player.vimeo.com/api/player.js"></script>
   </main>
+
+
 </template>
 
 <script>
