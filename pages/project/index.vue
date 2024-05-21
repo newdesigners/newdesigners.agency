1e2c1ff6d4368ac12f135654a934af6ec21eedab
Aaron_ <152491862+100542@users.noreply.github.com>
2024-05-21 17:08:03 +0200

    Aaron website #1

---
+++ pages/project/index.vue
@@ -0,0 +1,219 @@
+<template>
+  <main class="max-w-[1440px]">
+    <section id="section1">
+      <div class="-mt-6">
+        <div class="flex justify-center">
+          <img
+            class="absolute z-10 mt-5 content-center"
+            id="hsif"
+            src="../../static/svg/Group 81.png"
+            alt="HSiF"
+          />
+        </div>
+        <img
+          id="img1"
+          class="w-full"
+          src="../../static/svg/Vector 189.png"
+          alt="Banner"
+        />
+      </div>
+    </section>
+    <section>
+      <div class="flex justify-center">
+        <div class="w-1/2">
+          <h1 class="text-6xl font-bold text-center">
+            THE HAGUE STUDENT INVESTMENT FUND
+          </h1>
+          <div class="flex flex-row justify-evenly mt-4 space-x-4">
+            <div
+              id="bluebox"
+              class="w-24 h-auto bg-tertiary flex flex-col items-start justify-center"
+            >
+              <div class="flex items-center">
+                <img
+                  src="../../static/svg/pointing 3.svg"
+                  alt="Pointer"
+                  class="ml-5"
+                />
+                <h1 class="text-3xl ml-2 text-white">UX/UI</h1>
+              </div>
+              <div class="flex items-center mt-1">
+                <img
+                  src="../../static/svg/pointing 3.svg"
+                  alt="Pointer"
+                  class="ml-5"
+                />
+                <h1 class="text-3xl ml-2 text-white">Web Design</h1>
+              </div>
+              <div class="flex items-center mt-1">
+                <img
+                  src="../../static/svg/pointing 3.svg"
+                  alt="Pointer"
+                  class="ml-5"
+                />
+                <h1 class="text-3xl ml-2 text-white">Research</h1>
+              </div>
+              <div class="flex items-center mt-1">
+                <img
+                  src="../../static/svg/pointing 3.svg"
+                  alt="Pointer"
+                  class="ml-5"
+                />
+                <h1 class="text-3xl ml-2 text-white">8 weeks</h1>
+              </div>
+            </div>
+            <div>
+              <h1 class="text-4xl font-bold">Background</h1>
+              <p class="text-2xl">
+                It is a startup company working to help students and young
+                people in the field. They presented us with a project aimed at
+                creating their own website to introduce their customers to them.
+              </p>
+              <h1 class="text-4xl font-bold mt-3">Goal</h1>
+              <p class="text-2xl">
+                We did an in-depth study of all users and then we created
+                prototypes and tested them. Based on the results, we created the
+                final design and tested it again. It took us around 8 weeks to
+                finish this project, and we are glad to share the steps with
+                you!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="flex justify-center">
+        <img src="../../static/svg/Vector 185.png" alt="1" />
+        <img src="../../static/svg/Vector 188.png" alt="2" />
+        <img src="../../static/svg/Vector 187.png" alt="3" />
+      </div>
+      <div class="flex justify-center mt-2">
+        <img src="../../static/svg/Frame 478.png" alt="4" />
+      </div>
+      <div class="flex justify-center mt-7">
+        <img src="../../static/svg/Frame 461.png" alt="Process" />
+      </div>
+    </section>
+    <section>
+      <div class="bg-primary w-full h-24 flex relative">
+        <img src="../../static/svg/Vector 172.png" alt="border" />
+        <div class="ml-10 mt-3">
+          <h1 class="text-5xl font-bold ml-2 text-white">
+            Speed up your digital growth together?
+          </h1>
+          <p class="text-2xl ml-2 mt-1 text-white">
+            You can leave your project information, we will review it, and we
+            will contact you! After that we will have a free discussion of your
+            project.
+          </p>
+        </div>
+        <img
+          class="ml-10 absolute top-3 right-6"
+          src="../../static/svg/Group 83.png"
+          alt="border2"
+        />
+        <button
+          class="w-20 h-6 rounded-lg mt-12 absolute right-16 bg-secondary"
+        >
+          <h1 class="text-2xl">Request Project</h1>
+        </button>
+      </div>
+    </section>
+    <section>
+      <div class="ml-24">
+        <img src="../../static/svg/Services.png" alt="Similar Cases" />
+      </div>
+      <div>
+        <div class="ml-24 mt-12 flex flex-row">
+          <div class="bg-tertiary h-2 w-4 rounded-lg flex justify-center">
+            <p class="text-white">UX/UI</p>
+          </div>
+          <div class="bg-tertiary h-2 w-7 rounded-lg flex justify-center ml-1">
+            <p class="text-white">Web Design</p>
+          </div>
+          <div class="bg-tertiary h-2 w-6 rounded-lg flex justify-center ml-1">
+            <p class="text-white">Research</p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h1 class="ml-24 mt-2 text-3xl font-bold">HAAGS GEMEENTEARCHIEF</h1>
+            <div class="flex flex-row justify-evenly">
+              <p class="ml-1 text-2xl">
+                This company has had many problems with their website. They
+                asked for our help to improve these problems. So we studied the
+                case and studied the users and re-designed the website to
+                achieve the goals of the users.
+                <img
+                  class="ml-6 mt-2 h-4 w-auto"
+                  src="../../static/svg/Read more.png"
+                  alt="Read More"
+                />
+              </p>
+              <img
+                src="../../static/svg/Gemeente Haag Archief 1.png"
+                alt="Gemeente Haag"
+              />
+            </div>
+          </div>
+          <div class="ml-0 flex justify-evenly"></div>
+        </div>
+      </div>
+    </section>
+    <section class="flex justify-end mr-24 -mt-16">
+      <div>
+        <div class="right-24 mt-12 flex flex-row">
+          <div class="bg-tertiary h-2 w-5 rounded-lg flex justify-center">
+            <p class="text-white">Branding</p>
+          </div>
+        </div>
+        <h1 class="text-3xl mt-2 font-bold">COLOSSEUS</h1>
+        <p class="ml-0 text-2xl">
+          It is a data analytics company, we have given them a new identity
+          based on the goals and ambitions they want to reach. We have used the
+          same colors, but in an innovative way and in new shapes
+          <img
+            class="ml-6 mt-2 h-4 w-auto"
+            src="../../static/svg/Read more.png"
+            alt="Read More"
+          />
+        </p>
+        <img
+          class="absolute left-24 -mt-24"
+          src="../../static/svg/Colosseus 1.png"
+          alt="Gemeente Haag"
+        />
+      </div>
+    </section>
+  </main>
+</template>
+
+<style>
+#img1 {
+  max-height: 677px;
+  clip-path: polygon(0 0, 100% 0%, 100% 72.5%, 0% 100%);
+}
+
+#bluebox {
+  clip-path: polygon(0 0, 100% 4%, 100% 89%, 10% 100%);
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+}
+
+#orangeborder {
+  clip-path: polygon(0 0, 88% 25%, 68% 100%, 0% 100%);
+}
+
+</style>
+
+<script>
+export default {
+  head() {
+    return {
+      script: [
+        { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" }
+      ]
+    };
+  }
+};
+</script>
