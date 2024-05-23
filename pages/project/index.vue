559f97d16a07836d0228fdcfa9fce1556ecf056c
Aaron_ <152491862+100542@users.noreply.github.com>
2024-05-23 17:16:06 +0200

    Laatste commit 🎉

--- pages/project/index.vue
+++ pages/project/index.vue
@@ -10,6 +10,10 @@
             alt="HSiF"
           />
         </div>
+        <img 
+          class="absolute z-10 ml-3 mt-4 lg:mt-4 lg:ml-10"
+          src="../../static/svg/Back.png"
+          alt="Back" />
         <img
           id="img1"
           class="w-full h-24 lg:h-auto"
@@ -25,73 +29,91 @@
             THE HAGUE STUDENT INVESTMENT FUND
           </h1>
           <div class="flex flex-row justify-evenly">
-          <div class="flex flex-col justify-between md:flex-row mt-4 space-x-4">
             <div
-              id="bluebox"
-                class="-ml-2.25 w-16 h-20 lg:h-24 lg:w-24 bg-tertiary flex flex-col items-start justify-center"
+              class="flex flex-col justify-between lg:flex-row mt-4 space-x-4"
             >
-              <div class="flex items-center">
-                <img
-                  src="../../static/svg/pointing 3.svg"
-                  alt="Pointer"
-                  class="ml-1 lg:ml-3"
-                />
-                <h1 class="text-3xl ml-2 text-white">UX/UI</h1>
+              <div
+                id="bluebox"
+                class="-ml-2.25 w-fit pl-3 aspect-square h-20 lg:h-24 bg-tertiary flex flex-col items-start justify-center"
+              >
+                <div class="flex items-center">
+                  <img
+                    src="../../static/svg/pointing 3.svg"
+                    alt="Pointer"
+                    class="ml-1 lg:ml-3"
+                  />
+                  <h1 class="text-3xl ml-2 text-white">UX/UI</h1>
+                </div>
+                <div class="flex items-center mt-1">
+                  <img
+                    src="../../static/svg/pointing 3.svg"
+                    alt="Pointer"
+                    class="ml-1 lg:ml-3"
+                  />
+                  <h1 class="text-3xl ml-2 text-white whitespace-nowrap">Web Design</h1>
+                </div>
+                <div class="flex items-center mt-1">
+                  <img
+                    src="../../static/svg/pointing 3.svg"
+                    alt="Pointer"
+                    class="ml-1.5 lg:ml-3"
+                  />
+                  <h1 class="text-3xl ml-2 text-white">Research</h1>
+                </div>
+                <div class="flex items-center mt-1">
+                  <img
+                    src="../../static/svg/pointing 3.svg"
+                    alt="Pointer"
+                    class="ml-2 lg:ml-3"
+                  />
+                  <h1 class="text-3xl ml-2 text-white">8 weeks</h1>
+                </div>
               </div>
-              <div class="flex items-center mt-1">
-                <img
-                  src="../../static/svg/pointing 3.svg"
-                  alt="Pointer"
-                  class="ml-1 lg:ml-3"
-                />
-                <h1 class="text-3xl ml-2 text-white">Web Design</h1>
+              <div class="lg:flex flex-col justify-center w-full">
+                <h1 class="text-4xl mt-3 -ml-5 lg:ml-0 lg:mt-0 font-bold">
+                  Background
+                </h1>
+                <p class="text-2xl -ml-5 lg:ml-0">
+                  It is a startup company working to help students and young
+                  people in the field. They presented us with a project aimed at
+                  creating their own website to introduce their customers to
+                  them.
+                </p>
+                <h1 class="text-4xl -ml-5 lg:ml-0 font-bold mt-3">Goal</h1>
+                <p class="text-2xl -ml-5 lg:ml-0">
+                  We did an in-depth study of all users and then we created
+                  prototypes and tested them. Based on the results, we created
+                  the final design and tested it again. It took us around 8
+                  weeks to finish this project, and we are glad to share the
+                  steps with you!
+                </p>
               </div>
-              <div class="flex items-center mt-1">
-                <img
-                  src="../../static/svg/pointing 3.svg"
-                  alt="Pointer"
-                  class="ml-1.5 lg:ml-3"
-                />
-                <h1 class="text-3xl ml-2 text-white">Research</h1>
-              </div>
-              <div class="flex items-center mt-1">
-                <img
-                  src="../../static/svg/pointing 3.svg"
-                  alt="Pointer"
-                  class="ml-2 lg:ml-3"
-                />
-                <h1 class="text-3xl ml-2 text-white">8 weeks</h1>
-              </div>
-            </div>
-            <div class="md:flex flex-col justify-center w-full">
-              <h1 class="text-4xl mt-3 -ml-5 lg:ml-0 lg:mt-0 font-bold">Background</h1>
-              <p class="text-2xl -ml-5 lg:ml-0">
-                It is a startup company working to help students and young
-                people in the field. They presented us with a project aimed at
-                creating their own website to introduce their customers to them.
-              </p>
-              <h1 class="text-4xl -ml-5 lg:ml-0 font-bold mt-3">Goal</h1>
-              <p class="text-2xl -ml-5 lg:ml-0">
-                We did an in-depth study of all users and then we created
-                prototypes and tested them. Based on the results, we created the
-                final design and tested it again. It took us around 8 weeks to
-                finish this project, and we are glad to share the steps with
-                you!
-              </p>
             </div>
           </div>
         </div>
       </div>
-      </div>
     </section>
     <section>
       <div class="flex flex-row justify-center">
-        <img class="w-7 h-7 lg:h-auto lg:w-auto" src="../../static/svg/Vector 185.png" alt="1" />
-        <img class="w-7 h-7 lg:h-auto lg:w-auto" src="../../static/svg/Vector 188.png" alt="2" />
-        <img class="w-7 h-7 lg:h-auto lg:w-auto" src="../../static/svg/Vector 187.png" alt="3" />
+        <img
+          class="w-7 h-7 lg:h-auto lg:w-auto"
+          src="../../static/svg/Vector 185.png"
+          alt="1"
+        />
+        <img
+          class="w-7 h-7 lg:h-auto lg:w-auto"
+          src="../../static/svg/Vector 188.png"
+          alt="2"
+        />
+        <img
+          class="w-7 h-7 lg:h-auto lg:w-auto"
+          src="../../static/svg/Vector 187.png"
+          alt="3"
+        />
       </div>
       <div class="flex justify-center mt-2">
-        <p class="text-2xl font-bold">Visit website</p><img class="w-2 h-2" src="../../static/svg/akar-icons_arrow-down.png">
+        <p class="text-2xl font-bold">Visit website</p>
+        <img class="w-2 h-2" src="../../static/svg/akar-icons_arrow-down.png" />
       </div>
       <div class="flex justify-center mt-7">
         <img src="../../static/svg/Frame 461.png" alt="Process" />
@@ -99,10 +121,14 @@
     </section>
     <section>
       <div class="bg-primary w-full h-24 flex justify-center relative">
-        <img class="absolute bottom-0 left-0" src="../../static/svg/Vector 172.png" alt="border" />
+        <img
+          class="absolute bottom-0 left-0"
+          src="../../static/svg/Vector 172.png"
+          alt="border"
+        />
         <div class="z-10 lg:ml-10 mt-3">
           <h1 class="text-2xl lg:text-5xl lg:font-bold ml-2 text-white">
-        Speed up your digital growth together?
+            Speed up your digital growth together?
           </h1>
           <p class="text-mds lg:text-2xl ml-2 mt-1 text-white">
             You can leave your project information, we will review it, and we
@@ -116,71 +142,102 @@
           alt="border2"
         />
         <button
-          class="btn w-8 h-6 lg:w-20 lg:h-6 rounded-lg mt-13 absolute right-14 lg:right-16  bg-secondary"
+          class="btn w-fit h-6 lg:w-20 lg:h-6 rounded-lg mt-13 absolute right-14 lg:right-16  bg-secondary"
         >
-          <h1 class="text-2xl">Request Project</h1>
+          <h1 class="text-2xl whitespace-nowrap">Request Project</h1>
         </button>
       </div>
     </section>
     <section>
-  <div class="ml-2 lg:ml-24">
-    <img src="../../static/svg/Services.png" alt="Similar Cases" />
-  </div>
-<div class="flex flex-col lg:flex-row justify-evenly">
-  <div>
-    <div class="lg:ml-16 mt-2 lg:mt-6 flex flex-row">
-      <div class="bg-tertiary mt-0.5 lg:mt-0 ml-1 lg:ml-0 h-2 w-4 rounded-lg flex justify-center">
-        <p class="text-white">UX/UI</p>
+      <div class="ml-2 lg:ml-24">
+        <img src="../../static/svg/Services.png" alt="Similar Cases" />
       </div>
-      <div class="bg-tertiary mt-0.5 lg:mt-0 h-2 w-7 rounded-lg flex justify-center ml-1">
-        <p class="text-white">Web Design</p>
-      </div>
-      <div class="bg-tertiary mt-0.5 lg:mt-0 h-2 w-6 rounded-lg flex justify-center ml-1">
-        <p class="text-white">Research</p>
-      </div>
-    </div>
-    <div class="flex flex-col lg:flex-row justify-between items-start lg:items-center">
-      <div class="flex-grow">
-        <h1 class="ml-1 lg:ml-16 lg:mt-2 text-2xl font-bold">HAAGS GEMEENTEARCHIEF</h1>
-        <p class="ml-1 lg:ml-16 mt-2 lg:mt-0 text-2xl">
-          This company has had many problems with their website. They asked for our help to improve these problems. So we studied the case and studied the users and re-designed the website to achieve the goals of the users.
-        </p>
-        <h1 class="ml-1 lg:ml-16 text-2xl font-bold">Read More
-          <img class="-mt-2 ml-8 w-2 h-2" src="../../static/svg/akar-icons_arrow-down.png">
-        </h1>
-      </div>
-    </div>
-  </div>
+      <div class="flex flex-col lg:flex-row justify-evenly">
+        <div>
+          <div class="lg:ml-16 mt-2 lg:mt-6 flex flex-row">
+            <div
+              class="bg-tertiary items-center mt-0.5 lg:mt-0 ml-1 lg:ml-0 h-2 w-4 rounded-lg flex justify-center"
+            >
+              <p class="text-white">UX/UI</p>
+            </div>
+            <div
+              class="bg-tertiary items-center mt-0.5 lg:mt-0 h-2 w-7 rounded-lg flex justify-center ml-1"
+            >
+              <p class="text-white">Web Design</p>
+            </div>
+            <div
+              class="bg-tertiary items-center mt-0.5 lg:mt-0 h-2 w-6 rounded-lg flex justify-center ml-1"
+            >
+              <p class="text-white">Research</p>
+            </div>
+          </div>
+          <div
+            class="flex flex-col lg:flex-row justify-between items-start lg:items-center"
+          >
+            <div class="flex-grow">
+              <h1 class="ml-1 lg:ml-16 lg:mt-2 text-2xl font-bold">
+                HAAGS GEMEENTEARCHIEF
+              </h1>
+              <p class="ml-1 lg:ml-16 mt-2 lg:mt-0 text-2xl">
+                This company has had many problems with their website. They
+                asked for our help to improve these problems. So we studied the
+                case and studied the users and re-designed the website to
+                achieve the goals of the users.
+              </p>
+              <h1 class="ml-1 lg:ml-16 text-2xl font-bold">
+                Read More
+                <img
+                  class="-mt-2 ml-8 w-2 h-2"
+                  src="../../static/svg/akar-icons_arrow-down.png"
+                />
+              </h1>
+            </div>
+          </div>
+        </div>
 
-  <div class="mt-3 lg:mr-16 lg:mt-0">
-    <img src="../../static/svg/Gemeente Haag Archief 1.png" alt="Gemeente Haag" class="h-12 w-screen gemeente lg:w-24 lg:h-24" />
-  </div>
-</div>
-</section>
-<section>
-  <div class="flex flex-col lg:flex-row justify-evenly">
-    <div class="lg:w-1/2 lg:pr-6">
-      <img class="mt-3 h-12 colosseus w-screen lg:h-24 lg:w-24 lg:mt-0 lg:ml-12" src="../../static/svg/Colosseus 1.png" alt="Colosseus" />
-    </div>
-
-    <div class="lg:w-1/2 lg:pl-6">
-      <div class="ml-1 mt-2 lg:mt-6 flex flex-row">
-        <div class="bg-tertiary mt-0.5 lg:mt-0 h-2 w-5 rounded-lg flex justify-center">
-          <p class="text-white">Branding</p>
+        <div class="mt-3 lg:mr-16 lg:mt-0">
+          <img
+            src="../../static/svg/Gemeente Haag Archief 1.png"
+            alt="Gemeente Haag"
+            class="h-12 w-screen gemeente lg:w-24 lg:h-24"
+          />
         </div>
       </div>
-      <h1 class="text-3xl ml-1 mt-2 font-bold">COLOSSEUS</h1>
-      <p class="ml-1 mt-2 lg:mt-0 text-2xl">
-        It is a data analytics company, we have given them a new identity
-        based on the goals and ambitions they want to reach. We have used the
-        same colors, but in an innovative way and in new shapes.
-      </p>
-      <h1 class="text-2xl font-bold ml-1">Read More
-        <img class="-mt-2 ml-8 w-2 h-2" src="../../static/svg/akar-icons_arrow-down.png">
-      </h1>
-    </div>
-  </div>
-</section>
+    </section>
+    <section>
+      <div class="flex flex-col lg:flex-row justify-evenly">
+        <div class="lg:w-1/2 lg:pr-6">
+          <img
+            class="mt-3 h-12 colosseus w-screen lg:h-24 lg:w-24 lg:mt-0 lg:ml-12"
+            src="../../static/svg/Colosseus 1.png"
+            alt="Colosseus"
+          />
+        </div>
+
+        <div class="lg:w-1/2 lg:pl-6">
+          <div class="ml-1 mt-2 lg:mt-6 flex flex-row">
+            <div
+              class="bg-tertiary items-center mt-0.5 lg:mt-0 h-2 w-5 rounded-lg flex justify-center"
+            >
+              <p class="text-white">Branding</p>
+            </div>
+          </div>
+          <h1 class="text-3xl ml-1 mt-2 font-bold">COLOSSEUS</h1>
+          <p class="ml-1 mt-2 lg:mt-0 text-2xl">
+            It is a data analytics company, we have given them a new identity
+            based on the goals and ambitions they want to reach. We have used
+            the same colors, but in an innovative way and in new shapes.
+          </p>
+          <h1 class="text-2xl font-bold ml-1">
+            Read More
+            <img
+              class="-mt-2 ml-8 w-2 h-2"
+              src="../../static/svg/akar-icons_arrow-down.png"
+            />
+          </h1>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
@@ -192,7 +249,6 @@
 
 #bluebox {
   clip-path: polygon(0 0, 100% 4%, 100% 89%, 10% 100%);
-  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
 }
 
 #orangeborder {
@@ -219,9 +275,7 @@
     width: 500px;
     height: 300px;
   }
-  
 }
-
 </style>
 
 <script>
